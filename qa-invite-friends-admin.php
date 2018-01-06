a07d40299e72e2322dc69e27f68dd47dc2f2dd93
Mayur Shah <mail@mayurshah.in>
2018-01-07 00:39:53 +0530

    fixing messages

--- qa-invite-friends-admin.php
+++ qa-invite-friends-admin.php
@@ -8,9 +8,9 @@ class qa_invite_friends_admin
 
     switch($option) {
       case 'inf_subject':
-        return '0';
+        return '|*Name*| invites you to ' . qa_opt('site_title');
       case 'inf_message_body':
-        return 'Facebook Friends Invitation';
+        return 'Hi,<br />|*Name*| invites you to';
       default:
         return null;
     }
@@ -20,11 +20,16 @@ class qa_invite_friends_admin
   function admin_form(&$qa_content)
   {
     $saved=false;
+    $reset=false;
 
     if (qa_clicked('inf_save_button')) {
       qa_opt('inf_subject', qa_post_text('inf_subject_field'));
       qa_opt('inf_message_body', qa_post_text('inf_message_body_field'));
       $saved=true;
+    }else if (qa_clicked('inf_reset_button')) {
+      qa_opt('inf_subject', $this -> option_default('inf_subject'));
+      qa_opt('inf_message_body', $this -> option_default('inf_message_body'));
+      $reset = true;
     }
 
     return array(
@@ -35,11 +40,13 @@ class qa_invite_friends_admin
           'label' => 'Enter your subject.',
           'value' => qa_html(qa_opt('inf_subject')),
           'tags' => 'name="inf_subject_field"',
+          'note' => '<b>Insert |*Name*| in your sentence to insert the name of the user inviting.</b>',
         ),
         array(
           'label' => 'Enter your message. HTML Allowed: ',
           'value' => qa_html(qa_opt('inf_message_body')),
           'tags' => 'name="inf_message_body_field"',
+          'note' => '<b>Insert |*Name*| in your sentence to insert the name of the user inviting.</b>',
         ),
         array(
           'label' => 'Preview:<br />',
@@ -53,6 +60,10 @@ class qa_invite_friends_admin
           'label' => 'Save Changes',
           'tags' => 'name="inf_save_button"',
         ),
+        array(
+					'label' => 'Reset',
+					'tags' => 'NAME="inf_reset_button"',
+					),
       ),
     );
   }
